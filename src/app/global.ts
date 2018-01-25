ee55d493d2ff376e0420c2802483edb504baa4f2
ciocanmihaialexandru <ciocanmihaialexandru@users.noreply.github.com>
2018-01-25 19:16:42 +0200

    Update global.ts

--- src/app/global.ts
+++ src/app/global.ts
@@ -4,5 +4,5 @@
 
 export const GlobalVariable = Object.freeze({
   BASE_URL: 'http://35.198.85.24:8080',
-  API_URL: 'http://localhost:8080/store/api/horoscope',
+  API_URL: 'http://35.198.85.24:8080/store/api/horoscope',
 });
